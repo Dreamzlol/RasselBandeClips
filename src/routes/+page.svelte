0458cccb4867acd5a8a4856e2efd6fc5e0b0099b
Dreamzlol <denis.dasilvarocha@icloud.com>
2024-04-21 02:23:31 +0200

    Added Footer and Crown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import Arrow from '$lib/components/icons/arrow.svelte'
 	import Heart from '$lib/components/icons/heart.svelte'
 	import Crown from '$lib/components/icons/crown.svelte'
+	import Coffee from '$lib/components/icons/coffee.svelte'
 
 	const scrollToClips = () => {
 		const section = document.getElementById('clipsOfTheMonth')
@@ -23,7 +24,7 @@
 <div class="container flex flex-col self-center">
 	<div
 		class="flex h-screen flex-col items-center justify-center"
-		use:reveal={{ duration: 700, threshold: 0.1, reset: true }}
+		use:reveal={{ duration: 700, reset: true }}
 	>
 		<div class="relative px-6 pt-14 lg:px-8">
 			<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
@@ -47,20 +48,17 @@
 	</div>
 
 	<div class="flex h-screen flex-col items-center justify-center" id="clipsOfTheMonth">
-		<div
-			class="flex items-center justify-center"
-			use:reveal={{ duration: 700, threshold: 0.1, reset: true }}
-		>
+		<div class="flex items-center justify-center" use:reveal={{ duration: 700, reset: true }}>
 			<h2
-				class="bg-gradient-to-t from-cyan-600 to-blue-600 bg-clip-text text-5xl font-bold tracking-tight text-transparent"
+				class="bg-gradient-to-t from-cyan-600 to-blue-600 bg-clip-text text-4xl font-bold tracking-tight text-transparent md:text-6xl"
 			>
 				Clips of the Month
 			</h2>
 		</div>
 
 		<div
-			class="flex flex-col items-center justify-center pt-28"
-			use:reveal={{ duration: 700, threshold: 0.1, reset: true }}
+			class="flex flex-col items-center justify-center pt-14 md:pt-28"
+			use:reveal={{ duration: 700, reset: true }}
 		>
 			<div class="flex flex-wrap justify-center gap-8 p-4">
 				<Streamer
@@ -85,7 +83,7 @@
 
 		<div
 			class="flex flex-col items-center justify-center"
-			use:reveal={{ duration: 700, threshold: 0.1, reset: true }}
+			use:reveal={{ duration: 700, reset: true }}
 		>
 			<div class="flex flex-wrap justify-center gap-8 p-4">
 				<Streamer
@@ -114,7 +112,7 @@
 
 		<div
 			class="flex flex-col items-center justify-center"
-			use:reveal={{ duration: 700, threshold: 0.1, reset: true }}
+			use:reveal={{ duration: 700, reset: true }}
 		>
 			<div class="flex flex-wrap justify-center gap-8 p-4">
 				<Streamer
@@ -140,64 +138,35 @@
 
 	<div
 		class="flex h-screen flex-col items-center justify-center"
-		use:reveal={{ duration: 700, threshold: 0.1, reset: true }}
+		use:reveal={{ duration: 700, reset: true }}
 	>
-		<div class="flex items-center justify-center">
+		<div class="relative flex items-center justify-center">
 			<h2
-				class="bg-gradient-to-t from-yellow-700 to-yellow-200 bg-clip-text text-5xl font-bold tracking-tight text-transparent"
+				class="bg-gradient-to-t from-yellow-700 to-yellow-200 bg-clip-text text-4xl font-bold tracking-tight text-transparent md:text-6xl"
 			>
 				Hall of Fame
 			</h2>
+			<Crown class="absolute right-0 top-0 -mt-6 mr-4 md:mr-6 md:-mt-10 flex h-10 w-10 md:h-16 md:w-16 items-end justify-end" />
 		</div>
 	</div>
 </div>
 
-<div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-	<div
-		class="mt-16 border-t border-primary/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between"
-	>
-		<div>
-			<h3 class="text-sm font-semibold leading-6 text-primary">Subscribe to our newsletter</h3>
-			<p class="mt-2 text-sm leading-6 text-primary">
-				The latest news, articles, and resources, sent to your inbox weekly.
-			</p>
+<footer>
+	<div class="flex flex-col items-center justify-center py-12">
+		<div class="flex items-center justify-center">
+			<p class="pr-2 text-white">Made with</p>
+			<Heart class="h-6 w-6" />
+			<span class="px-2 text-white">and</span>
+			<Coffee class="h-6 w-6" />
 		</div>
-		<form class="mt-6 sm:flex sm:max-w-md lg:mt-0">
-			<label for="email-address" class="sr-only">Email address</label>
-
-			<div class="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-				<Button
-					type="submit"
-					class="flex w-full items-center justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold "
-					>Subscribe</Button
-				>
-			</div>
-		</form>
-	</div>
-</div>
-
-<footer aria-labelledby="footer-heading">
-	<h2 id="footer-heading" class="sr-only">Footer</h2>
-	<div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-		<div class="xl:grid xl:grid-cols-1 xl:gap-8">
-			<div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-				<div class="md:grid md:grid-cols-2 md:gap-8">
-					<div class="hidden"></div>
-					<div class="mt-10 hidden md:mt-0"></div>
-				</div>
-				<div class="md:grid md:grid-cols-2 md:gap-8">
-					<div class="hidden"></div>
-					<div class="mt-10 md:mt-0">
-						<h3 class="text-sm font-semibold leading-6 text-primary">Legal</h3>
-						<ul role="list" class="mt-6 space-y-4"></ul>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="mt-8 border-t border-primary/10 pt-8 md:flex md:items-center md:justify-between">
-			<div class="flex space-x-6 md:order-2"></div>
-			<p class="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0"></p>
+		<div class="flex items-center justify-center">
+			<p class="pr-2 text-white">by</p>
+			<a
+				href="https://www.twitch.tv/dreams___________"
+				class="bg-gradient-to-t from-blue-700 to-blue-400 bg-clip-text font-bold text-transparent"
+			>
+				Dreams___________
+			</a>
 		</div>
 	</div>
 </footer>

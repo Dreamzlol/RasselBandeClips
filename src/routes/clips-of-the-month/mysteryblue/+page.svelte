d4e591cb2bd4c9cbc27a44280a8645c7ccc26f8b
Dreamzlol <denis.dasilvarocha@icloud.com>
2024-04-28 15:24:23 +0200

    Trigger Pipeline

--- src/routes/clips-of-the-month/mysteryblue/+page.svelte
+++ src/routes/clips-of-the-month/mysteryblue/+page.svelte
@@ -2,4 +2,8 @@
 	import ClipsOfMonth from '$lib/components/clipsOfTheMonth/Streamer.svelte'
 </script>
 
-<ClipsOfMonth amount="6" username="Mystery_Blue" id="90631404" />
+<ClipsOfMonth
+	amount="6" 
+	username="Mystery_Blue" 
+	id="90631404" 
+	/>
